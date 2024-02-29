d99fa61901127c7f60e2832130e1ca4af3bd23e0
Joshua Magady <josh.magady@gmail.com>
2024-02-29 04:34:13 -0600

    docs: Add docstrings to storage.py classes
    Added concise and comprehensive docstrings to the CommitDataStorage and JsonCommitDataStorage classes in storage.py. The docstrings explain the purpose of each class, their methods, parameters, return values, and included some usage examples.

--- src/gitgrazer/storage.py
+++ src/gitgrazer/storage.py
@@ -3,6 +3,11 @@ import os
 from abc import ABC, abstractmethod
 
 class CommitDataStorage(ABC):
+    """
+
+    A class for storing commit data.
+
+    """
     @abstractmethod
     def save(self, commit_hash):
         pass
@@ -12,6 +17,41 @@ class CommitDataStorage(ABC):
         pass
 
 class JsonCommitDataStorage(CommitDataStorage):
+    """
+    This class is a implementation of the `CommitDataStorage` interface and provides functionality to store and retrieve processed commit data in a JSON file.
+
+    :class: `JsonCommitDataStorage`
+
+    Methods:
+        - __init__(self, filepath='processed_commits.json'):
+            Initializes a new instance of the `JsonCommitDataStorage` class.
+            Parameters:
+                - filepath (str): The path to the JSON file to be used for storage. If not provided, the default value is 'processed_commits.json'.
+
+        - _load(self):
+            Loads the processed commits from the JSON file.
+            Returns:
+                - set: A set of processed commit hashes.
+
+        - save(self, commit_hash):
+            Adds a commit hash to the list of processed commits and saves it to the JSON file.
+            Parameters:
+                - commit_hash (str): The commit hash to be added and saved.
+
+        - has_commit(self, commit_hash):
+            Checks if a commit hash is present in the processed commits.
+            Parameters:
+                - commit_hash (str): The commit hash to be checked.
+            Returns:
+                - bool: True if the commit hash is present, False otherwise.
+
+    Example usage:
+        storage = JsonCommitDataStorage('processed_commits.json')
+        storage.save('commit_1')
+        storage.save('commit_2')
+        print(storage.has_commit('commit_1'))  # Output: True
+        print(storage.has_commit('commit_3'))  # Output: False
+    """
     def __init__(self, filepath='processed_commits.json'):
         self.filepath = filepath
         self._processed_commits = self._load()
