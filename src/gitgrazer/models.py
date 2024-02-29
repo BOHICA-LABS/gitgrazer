8c5af2e82cfe90c6ec4fee3acd1a9eb12ae5a31b
Joshua Magady <josh.magady@gmail.com>
2024-02-29 06:38:41 -0600

    feat: Add new classes and attributes in models.py
    Expanded the CommitOutput class in models.py by adding the 'diff_summary' attribute and created a new class called ChangeDescription. The changes aim to provide a more comprehensive representation of each commit and change description within the GitGrazer application.

--- src/gitgrazer/models.py
+++ src/gitgrazer/models.py
@@ -1,9 +1,37 @@
 from pydantic import BaseModel
 from typing import Any
 
+
 class CommitOutput(BaseModel):
+    """
+    Represents the output of a commit operation.
+
+    Attributes:
+        commit_hash (str): The hash of the commit.
+        author (str): The name of the author of the commit.
+        date (Any): The date of the commit.
+        message (str): The commit message.
+        change_description (str, optional): The description of the changes made in the commit.
+
+    """
     commit_hash: str
     author: str
     date: Any
     message: str
     change_description: str = None
+    diff_summary: str = None
+
+
+class ChangeDescription(BaseModel):
+    """
+    ChangeDescription Class
+
+    A class for representing a change description.
+
+    Attributes:
+        content (str): The content of the change description.
+        diff_summary (str): A summary of the difference made by the change.
+
+    """
+    content: str
+    diff_summary: str
